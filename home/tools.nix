b4349e2524217010813d1cc1b6ceb0e0d1168097
S. Brent Faulkner <sbfaulkner@gmail.com>
2026-05-10 16:37:08 -0400

    tools: add nodejs, set npm global prefix

--- home/tools.nix
+++ home/tools.nix
@@ -5,5 +5,12 @@
     fd       # fast alternative to find
     gh       # GitHub CLI
     ejson    # encrypted secrets management
+    nodejs   # javascript runtime (+ npm)
   ];
+
+  # Set npm's global package prefix to a user-owned directory.
+  # Without this, `npm install -g` would try to write into the Nix store (read-only).
+  home.sessionVariables = {
+    NPM_CONFIG_PREFIX = "$HOME/.npm-global";
+  };
 }
